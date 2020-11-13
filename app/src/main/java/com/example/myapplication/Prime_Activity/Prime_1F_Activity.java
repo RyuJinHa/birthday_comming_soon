8bef7f3d063cf4b7f2fb8fc6f63a3b763094e033
RyuJinHa <43023487+RyuJinHa@users.noreply.github.com>
2020-11-13 21:22:13 +0900

    11월 13일 오후 9시 반 수정
    1,2,3층 애니메이션이동,

--- app/src/main/java/com/example/myapplication/Prime_Activity/Prime_1F_Activity.java
+++ app/src/main/java/com/example/myapplication/Prime_Activity/Prime_1F_Activity.java
@@ -1,24 +1,293 @@
 package com.example.myapplication.Prime_Activity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
 
 public class Prime_1F_Activity extends AppCompatActivity {
 
-    TextView textView11;
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime_1f);
 
-        textView11 = findViewById(R.id.textView11);
+        moving_101();
+        moving_102();
+        moving_103();
+        moving_104();
+        moving_105();
+        moving_106();
+        moving_107();
 
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
+    // 101호에서 최단거리 이동
+    private void moving_101() {
+        final Button b = (Button) findViewById(R.id.p_101);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 1300, 1300, 1580); //x 좌표
+                        animation1.setDuration(1500);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 430, 350, 350); //y 좌표
+                        animation3.setDuration(1500);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0); //기존의 이미지 위치로 복귀
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    // 102호에서 최단거리 이동
+    private void moving_102() {
+        final Button b = (Button) findViewById(R.id.p_102);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX",  1150, 1300, 1300, 1580); //x 좌표
+                        animation1.setDuration(1600);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY",  430, 430, 350, 350); //y 좌표
+                        animation3.setDuration(1600);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0); //기존의 이미지 위치로 복귀
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    // 103호에서 최단거리 이동
+    private void moving_103() {
+        final Button b = (Button) findViewById(R.id.p_103);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX",  880, 1300, 1300, 1580); //x 좌표
+                        animation1.setDuration(1800);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY",  430, 430, 350, 350); //y 좌표
+                        animation3.setDuration(1800);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0); //기존의 이미지 위치로 복귀
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    //104호에서 최단거리 이동
+    private void moving_104() {
+        final Button button = (Button) findViewById(R.id.p_104);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX",  700, 700, 1300, 1300, 1580);
+                        animation1.setDuration(2100);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY",  600, 430, 430, 350, 350);
+                        animation3.setDuration(2100);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0);
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    //105호에서 최단거리 이동
+    private void moving_105() {
+        final Button button = (Button) findViewById(R.id.p_105);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 700, 1300, 1300, 1580);
+                        animation1.setDuration(1900);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY",  430, 430, 350, 350);
+                        animation3.setDuration(1900);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0);
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    //106호에서 최단거리 이동
+    private void moving_106() {
+        final Button button = (Button) findViewById(R.id.p_106);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 700,1300, 1300, 1580);
+                        animation1.setDuration(1900);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 430, 430, 350, 350);
+                        animation3.setDuration(1900);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0);
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    //107호에서 최단거리 이동
+    private void moving_107() {
+        final Button button = (Button) findViewById(R.id.p_107);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 890, 1300, 1300, 1580);
+                        animation1.setDuration(1800);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 430, 430, 350, 350);
+                        animation3.setDuration(1800);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0);
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/myapplication/Prime_Activity/Prime_2F_Activity.java
+++ app/src/main/java/com/example/myapplication/Prime_Activity/Prime_2F_Activity.java
@@ -28,10 +28,86 @@ public class Prime_2F_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime_2f);
 
+        moving_201();
+        moving_202();
         moving_203();
         moving_204();
+        moving_205();
+        moving_206();
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    // 201호에서 최단거리 이동
+    private void moving_201() {
+        final Button b = (Button) findViewById(R.id.p_201);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 1400, 1315, 1315); //x 좌표
+                        animation1.setDuration(1500);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 500, 500, 325); //y 좌표
+                        animation3.setDuration(1500);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0); //기존의 이미지 위치로 복귀
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    // 202호에서 최단거리 이동
+    private void moving_202() {
+        final Button b = (Button) findViewById(R.id.p_202);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 1100, 1315, 1315); //x 좌표
+                        animation1.setDuration(1500);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 500, 500, 325); //y 좌표
+                        animation3.setDuration(1500);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0); //기존의 이미지 위치로 복귀
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
     }
 
     // 203호에서 최단거리 이동
@@ -44,11 +120,11 @@ public class Prime_2F_Activity extends AppCompatActivity {
 
                 if (flag == 0) {
                     {
-                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 400, 400, 550, 550); //x 좌표
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 500, 762, 762); //x 좌표
                         animation1.setDuration(1500);
                         animation1.start();
 
-                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 400, 360, 360, 540); //y 좌표
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 500, 500, 800); //y 좌표
                         animation3.setDuration(1500);
                         animation3.start();
                     }
@@ -80,11 +156,83 @@ public class Prime_2F_Activity extends AppCompatActivity {
 
                 if (flag == 0) {
                     {
-                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 420, 420, 550, 550);
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 500, 762, 762);
                         animation1.setDuration(1500);
                         animation1.start();
 
-                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 300, 360, 360, 540);
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 500, 500, 800);
+                        animation3.setDuration(1500);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0);
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    //205호에서 최단거리 이동
+    private void moving_205() {
+        final Button button = (Button) findViewById(R.id.p_205);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 762, 762);
+                        animation1.setDuration(1500);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY",  500, 800);
+                        animation3.setDuration(1500);
+                        animation3.start();
+                    }
+                    flag = 1;
+                } else {
+                    {
+                        ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", 0);
+                        animation.setDuration(500);
+                        animation.start();
+
+                        ObjectAnimator animation2 = ObjectAnimator.ofFloat(iv, "translationY", 0);
+                        animation2.setDuration(500);
+                        animation2.start();
+                    }
+                    flag = 0;
+                }
+
+            }
+        });
+    }
+
+    //206호에서 최단거리 이동
+    private void moving_206() {
+        final Button button = (Button) findViewById(R.id.p_206);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (flag == 0) {
+                    {
+                        ObjectAnimator animation1 = ObjectAnimator.ofFloat(iv, "translationX", 900, 762, 762);
+                        animation1.setDuration(1500);
+                        animation1.start();
+
+                        ObjectAnimator animation3 = ObjectAnimator.ofFloat(iv, "translationY", 500, 500, 800);
                         animation3.setDuration(1500);
                         animation3.start();
                     }
